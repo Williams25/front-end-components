b383aef2e13836d19d8097d269fba676927513a7
Williams25 <william.souza.timao@hotmail.com>
2021-11-19 15:46:04 -0300

    criando templates pré definidos para components e pages

---
+++ generators/pages.js
@@ -0,0 +1,28 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "application controller",
+
+    // inquirer prompts
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "page name?",
+      },
+    ],
+
+    // actions to perform
+    actions: [
+      {
+        type: "add",
+        path: "../src/pages/{{pascalCase name}}/index.tsx",
+        templateFile: "../templates/pages/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/pages/{{pascalCase name}}/styles.module.css",
+        templateFile: "../templates/pages/styles.module.css.hbs",
+      },
+    ],
+  });
+};
