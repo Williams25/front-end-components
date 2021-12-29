30edab2565f8ea352b037c7afb49dce7e64851b2
Williams25 <william.souza.timao@hotmal.com>
2021-12-29 15:42:14 -0300

    criando collapse

---
+++ src/components/SideBar/Items/Collapse/index.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useCallback } from "react";
+import { Typography } from "@material-ui/core";
+import { useState, ReactNode } from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  useStyles,
+} from "./styles";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+type SideBarHref = {
+  href: string;
+  text: string;
+
+  paths: {
+    path: string;
+    type: "dynamic" | "static";
+  }[];
+};
+
+type CollapseProps = {
+  title: string;
+  sideBar: Array<SideBarHref>;
+  isLocal: string;
+  children: ReactNode;
+  icon?: () => JSX.Element;
+};
+
+export const Collapse = ({
+  isLocal,
+  sideBar,
+  title,
+  children,
+  icon,
+}: CollapseProps) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const classes = useStyles();
+
+  const handleClick = () => {
+    setExpanded((e) => !e);
+  };
+
+  const verificationPath = useCallback(() => {
+    const paths = sideBar.map((sideBar) => {
+      const isPathExists = sideBar.paths.filter((path) => {
+        if (path.type === "dynamic") {
+          const newPath = isLocal.split("/");
+          return path.path === isLocal.replace(newPath[newPath.length - 1], "");
+        }
+        if (path.type === "static") {
+          return path.path === isLocal;
+        }
+        return null;
+      }).length;
+      return isPathExists;
+    });
+
+    const newPath = paths.filter((path) => path > 0).length;
+
+    if (newPath > 0) {
+      setExpanded(true);
+    }
+  }, [isLocal]);
+
+  useEffect(() => {
+    verificationPath();
+  }, [isLocal]);
+
+  return (
+    <Accordion
+      square
+      expanded={expanded}
+      onChange={handleClick}
+      // TransitionComponent={() => <>{expanded ? children : null}</>}
+      className={classes.accordion}
+    >
+      <AccordionSummary
+        aria-controls="panel1d-content"
+        id="panel1d-header"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <Typography
+          style={{
+            fontSize: "0.75rem",
+            lineHeight: "2.5",
+            fontWeight: 700,
+            textTransform: "uppercase",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          {icon && <>{icon}</>}
+          <span style={{ marginLeft: icon && "0.5rem" }}>{title}</span>
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails style={{ padding: "0 !important" }}>
+        {children}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
---
+++ src/components/SideBar/Items/Collapse/styles.ts
@@ -0,0 +1,65 @@
+import { withStyles, makeStyles, Theme } from "@material-ui/core";
+import {
+  AccordionDetails as MuiAccordionDetails,
+  AccordionSummary as MuiAccordionSummary,
+  Accordion as MuiAccordion,
+} from "@material-ui/core";
+
+export const Accordion = withStyles({
+  root: {
+    overflow: "hidden",
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+    backgroundColor: "#fff",
+  },
+  expanded: {},
+})(MuiAccordion);
+
+export const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "#fff",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+    transition: "filter 0.2s",
+    "&:hover": {
+      filter: "brightness(96%)",
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+export const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: "0 !important",
+    display: "flex",
+    flexDirection: "column",
+  },
+}))(MuiAccordionDetails);
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  accordionDark: {
+    backgroundColor: "#212121",
+    "& svg": {
+      color: "#fafafa",
+    },
+  },
+  accordion: {},
+}));
