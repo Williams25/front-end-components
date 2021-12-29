dd7f925eb5290c3c6e93d97dd9cf99f0b02b5a01
Williams25 <william.souza.timao@hotmal.com>
2021-12-29 15:42:54 -0300

    passando theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SideBar } from "src/components";
 import { items } from "src/global/ItemsSideBar";
+import { MuiThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+
+export const primary = createTheme({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SideBar data={items}>
-      <Component {...pageProps} />
-    </SideBar>
+    <MuiThemeProvider theme={primary}>
+      <SideBar data={items}>
+        <Component {...pageProps} />
+      </SideBar>
+    </MuiThemeProvider>
   );
 }
 
