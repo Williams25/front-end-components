e29c168cd64bfe4c1777eba3cadda455041f8401
Williams25 <william.souza.timao@hotmail.com>
2021-12-30 16:42:32 -0300

    renderizando novo component (CardFlipHover)

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,9 +1,19 @@
 import styles from "src/styles/Cards/styles.module.css";
-import { Card, SyntaxHighlighter } from "src/components";
+import { Card, CardFlipHover } from "src/components";
 const Cards = () => {
   return (
     <div className={styles.container}>
       <Card variant_color_icon="#37ba82" description="Lorem ipsium" />
+      <CardFlipHover
+        images={{
+          image1:
+            "/assets/img/br-galaxy-watch4-398979-sm-r870nzkpzto-481128469.webp",
+          image2:
+            "/assets/img/br-galaxy-watch4-398979-sm-r870nzkpzto-481128460.webp",
+        }}
+        price="1.599,00"
+        title="Galaxy Watch4 BT 44mm - Preto"
+      />
     </div>
   );
 };
