65f2e1f8c25887e82344f8acfeb00a826efab248
Williams25 <william.souza.timao@hotmail.com>
2021-12-30 16:42:15 -0300

    exportando novo component (CardFlipHover)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { ShowTsxAndCss } from "src/components/ShowTsxAndCss";
 export { SideBar } from "src/components/SideBar";
 export { Button } from "src/components/Button";
 export { Card } from "src/components/Card";
+export { CardFlipHover } from "src/components/CardFlipHover";
