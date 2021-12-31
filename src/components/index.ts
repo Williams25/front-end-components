b832f332d430988023e1f92e7bb833d92d7b8a09
Williams25 <william.souza.timao@hotmail.com>
2021-12-31 16:33:45 -0300

    exportando CardHexagon

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { Button } from "src/components/Button";
 export { Card } from "src/components/Card";
 export { CardFlipHover } from "src/components/CardFlipHover";
 export { CardGlass } from "src/components/CardGlass";
+export { CardHexagon } from "src/components/CardHexagon";
