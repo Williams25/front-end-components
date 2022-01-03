064097deda0db47dcbd9c4d93b34feb68e96e884
Williams25 <william.souza.timao@hotmail.com>
2022-01-02 23:03:45 -0300

    exportando novo card CardGlassProduct

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { Card } from "src/components/Card";
 export { CardFlipHover } from "src/components/CardFlipHover";
 export { CardGlass } from "src/components/CardGlass";
 export { CardHexagon } from "src/components/CardHexagon";
+export { CardGlassProduct } from "src/components/CardGlassProduct";
