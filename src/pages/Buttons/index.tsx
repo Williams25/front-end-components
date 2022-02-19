51de449caddb6257e6a4d8ed55bd6647e189f3f2
Williams25 <william.souza.timao@hotmail.com>
2022-02-18 22:26:24 -0300

    ajustando pagina buttom

--- src/pages/Buttons/index.tsx
+++ src/pages/Buttons/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "src/components";
+import { Button, AnimateText } from "src/components";
 import styles from "src/styles/Buttons/styles.module.css";
 
 const Buttons = () => {
   return (
     <div className={styles.container}>
       <Button variant="button-dark" title="Button" />
+      <AnimateText />
     </div>
   );
 };
