b1aed43bd476c149c748d080f94edb0e2a1a6020
Williams25 <william.souza.timao@hotmail.com>
2022-02-18 22:26:33 -0300

    criando nova pagina

---
+++ src/pages/Typography/index.tsx
@@ -0,0 +1,12 @@
+import { AnimateText } from "src/components";
+import styles from "src/styles/Typography/styles.module.css";
+
+const Typography = () => {
+  return (
+    <div className={styles.container}>
+      <AnimateText />
+    </div>
+  );
+};
+
+export default Typography;
