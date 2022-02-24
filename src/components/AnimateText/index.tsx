6ff9be18351abd91cb0fa0ab8d4409f4c21d003f
Williams25 <william.souza.timao@hotmail.com>
2022-02-23 23:24:59 -0300

    ajustando component

--- src/components/AnimateText/index.tsx
+++ src/components/AnimateText/index.tsx
@@ -2,7 +2,7 @@ import { Content } from "./styles";
 export const AnimateText = () => {
   return (
     <Content>
-      <ul className={"menu"}>
+      <ul className="menu">
         <li>
           <a href="#" data-text="&nbsp;Home">
             &nbsp;Home&nbsp;
