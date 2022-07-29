6268a26b5739c1afc5eaa276cec014791d613d83
Williams25 <william.souza.timao@hotmail.com>
2022-07-29 14:52:50 -0300

    criando paginas para inputs

---
+++ src/pages/Inputs/index.tsx
@@ -0,0 +1,11 @@
+import { InputField } from "src/components";
+import styles from "src/styles/Input/styles.module.css";
+const Input = () => {
+  return (
+    <div className={styles.container}>
+      <InputField />
+    </div>
+  );
+};
+
+export default Input;
