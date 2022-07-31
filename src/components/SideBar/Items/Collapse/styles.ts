20e4c826f6fa85d2247fbffaa5110ccd39284ee1
Williams25 <william.souza.timao@hotmail.com>
2022-07-30 23:26:18 -0300

    criando estilos para collapse

--- src/components/SideBar/Items/Collapse/styles.ts
+++ src/components/SideBar/Items/Collapse/styles.ts
@@ -1,67 +1,85 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
-import { withStyles, makeStyles, Theme } from "@material-ui/core";
-import {
-  AccordionDetails as MuiAccordionDetails,
-  AccordionSummary as MuiAccordionSummary,
-  Accordion as MuiAccordion
-} from "@material-ui/core";
+import styled, { css } from "styled-components";
 
-export const Accordion = withStyles({
-  root: {
-    overflow: "hidden",
-    border: "1px solid rgba(0, 0, 0, .125)",
-    boxShadow: "none",
-    "&:not(:last-child)": {
-      borderBottom: 0
-    },
-    "&:before": {
-      display: "none"
-    },
-    "&$expanded": {
-      margin: "auto"
-    },
-    backgroundColor: "#fff"
-  },
-  expanded: {}
-})(MuiAccordion);
+export const Details = styled.details`
+  ${({ theme }) => css`
+    width: 100%;
+    padding: 1rem 0.5rem;
+    text-transform: uppercase;
+    color: ${theme.colors.white_100};
+    font-weight: 600;
+    font-size: 1rem;
+    transition: all 0.2s;
 
-export const AccordionSummary = withStyles({
-  root: {
-    backgroundColor: "#fff",
-    borderBottom: "1px solid rgba(0, 0, 0, .125)",
-    marginBottom: -1,
-    minHeight: 56,
-    "&$expanded": {
-      minHeight: 56
-    },
-    transition: "filter 0.2s",
-    "&:hover": {
-      filter: "brightness(96%)"
+    summary {
+      list-style: none;
+      cursor: pointer;
+      user-select: none;
     }
-  },
-  content: {
-    "&$expanded": {
-      margin: "12px 0"
-    }
-  },
-  expanded: {}
-})(MuiAccordionSummary);
 
-export const AccordionDetails = withStyles((theme: Theme) => ({
-  root: {
-    padding: "0 !important",
-    display: "flex",
-    flexDirection: "column"
-  }
-}))(MuiAccordionDetails);
-
-export const useStyles = makeStyles((theme: Theme) => ({
-  accordionDark: {
-    backgroundColor: "#212121",
-    "& svg": {
-      color: "#fafafa"
+    & + & {
+      border-top: 1px solid ${theme.colors.blue_600};
     }
-  },
-  accordion: {}
-}));
+
+    & .active-animation-details-summary {
+      animation: showDetailsSummary 0.2s forwards;
+    }
+    & .hidden-animation-details-summary {
+      animation: hiddenDetailsSummary 0.2s forwards;
+    }
+
+    & ul {
+      width: 100%;
+      display: flex;
+      gap: 0.5rem;
+      flex-direction: column;
+      margin-top: 1rem;
+
+      & li {
+        list-style: none;
+        width: inherit;
+
+        & a {
+          width: inherit;
+          display: block;
+          text-align: left;
+          padding: 0.5rem 1rem;
+          font-weight: 500;
+          font-size: 0.875rem;
+          color: ${theme.colors.white_200};
+          text-transform: uppercase;
+          cursor: pointer;
+          background-color: transparent;
+          border: 0;
+          outline: none;
+          transition: filter 0.2s;
+
+          &:hover {
+            filter: brightness(0.9);
+          }
+        }
+      }
+    }
+
+    @keyframes showDetailsSummary {
+      from {
+        transform: translate(0, -5px);
+        opacity: 0;
+      }
+      to {
+        transform: translate(0, 0);
+        opacity: 1;
+      }
+    }
+
+    @keyframes hiddenDetailsSummary {
+      from {
+        transform: translate(0, 0);
+        opacity: 1;
+      }
+      to {
+        transform: translate(0, -5px);
+        opacity: 0;
+      }
+    }
+  `}
+`;
