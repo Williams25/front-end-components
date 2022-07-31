48dca96ef5e12306a2c90c9f5f0292a2043f5cad
Williams25 <william.souza.timao@hotmail.com>
2022-07-30 23:25:14 -0300

    renderizando theme para a aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,20 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SideBar } from "src/components";
 import { items } from "src/global/ItemsSideBar";
-import { createTheme } from "@material-ui/core/styles";
 import { GlobalStyle } from "src/styles/global";
 import theme from "src/styles/theme";
 import { ThemeProvider } from "styled-components";
-export const primary = createTheme({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <SideBar data={items}>
-        <Component {...pageProps} />
-      </SideBar>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <SideBar data={items}>
+          <Component {...pageProps} />
+        </SideBar>
+      </ThemeProvider>
+    </>
   );
 }
 
