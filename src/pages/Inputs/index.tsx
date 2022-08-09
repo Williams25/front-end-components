cecc28445f1a099a4e657ef80e5963f546c64113
Williams25 <william.souza.timao@hotmail.com>
2022-08-09 20:33:07 -0300

    renderizando input

--- src/pages/Inputs/index.tsx
+++ src/pages/Inputs/index.tsx
@@ -1,10 +1,13 @@
-import { InputField } from "src/components";
+import { InputField, InputFieldBorder } from "src/components";
 
 const Input = () => {
   return (
     <div className="section-container-components">
       <div className="components-items">
         <InputField />
+      </div>
+      <div className="components-items">
+        <InputFieldBorder />
       </div>
     </div>
   );
